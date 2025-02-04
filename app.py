78e3058f0af01269036b436bb555d50870955d5f
mybear1109 <mybear8211@gmail.com>
2025-02-04 13:21:09 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from pandas.api.types import is_integer_dtype,is_float_dtype,is_object_dtype
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.compose import ColumnTransformer
 import matplotlib.pyplot as plt
-import matplotlib.font_manager as fm
+
 
 
 
@@ -17,7 +17,7 @@ import matplotlib.font_manager as fm
 
 @st.cache_data
 def fontRegistered():
-    font_dirs = [os.getcwd() + '/customFonts']
+    font_dirs = [os.getcwd() + '/custom_fonts']
     font_files = fm.findSystemFonts(fontpaths=font_dirs)
     for font_file in font_files:
         fm.fontManager.addfont(font_file)
@@ -31,9 +31,7 @@ def fontRegistered():
 def main():
 
     fontRegistered()
-    fontNames = [f.name for f in fm.fontManager.ttflist]
-    fontname = st.selectbox('NanumGothic', np.unique(fontNames))
-    plt.rc('font', family=fontname)
+    plt.rc('font', family='NanumGothic')
     
     st.title('K-Means Clustering APP')
 
